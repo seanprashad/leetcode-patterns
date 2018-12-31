bb35c46ec9272f30fc0fed4cb46652c82e4523a4
Sean Prashad <S.Prashad@outlook.com>
2018-12-31 18:36:06 -0500

    Add PathSum

---
+++ PathSum.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+
+        // If we're at a leaf node and the node's value give us a remaider of 0, aka
+        // we have found a path that adds up!
+        if (root.left == null && root.right == null && sum - root.val == 0) {
+            return true;
+        }
+
+        // Check the descendents, subtracting the values we've found so far from the
+        // total to find
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
