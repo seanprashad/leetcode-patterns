9fcd66424ec78099ef7a343ef0d30ca000762361
Sean Prashad <S.Prashad@outlook.com>
2018-12-31 00:52:27 -0500

    Add Recursive-PostOrder-Traversal

---
+++ Recursive-PostOrder-Traversal.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    ArrayList<Integer> nodes = new ArrayList<Integer>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return nodes;
+        }
+
+        if (root.left != null) {
+            postorderTraversal(root.left);
+        }
+        if (root.right != null) {
+            postorderTraversal(root.right);
+        }
+        nodes.add(root.val);
+        // postorderTraversal(root);
+
+        return nodes;
+    }
+}
