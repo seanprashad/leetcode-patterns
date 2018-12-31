88ee6c746e21679fa469533d6cd4b6bd6fdbae82
Sean Prashad <S.Prashad@outlook.com>
2018-12-30 23:37:12 -0500

    Add Recursive-InOrder-Traversal

---
+++ Recursive-InOrder-Traversal.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    ArrayList<Integer> nodes = new ArrayList<Integer>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return nodes;
+        }
+
+        nodes.add(root.val);
+
+        if (root.left != null) {
+            preorderTraversal(root.left);
+        }
+        if (root.right != null) {
+            preorderTraversal(root.right);
+        }
+
+        return nodes;
+    }
+}
