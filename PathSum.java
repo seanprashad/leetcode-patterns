553f7b07acc81fcfec3ad2f033d0476abb6c4d44
Sean Prashad <S.Prashad@outlook.com>
2018-12-31 21:22:57 -0500

    Rename class from Solution to PathSum

--- PathSum.java
+++ PathSum.java
@@ -2,7 +2,7 @@
  * Definition for a binary tree node. public class TreeNode { int val; TreeNode
  * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
-class Solution {
+class PathSum {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) {
             return false;
