14260611451910fdc42f1d16c25d77e28e0fc7e7
Sean Prashad <S.Prashad@outlook.com>
2019-01-01 23:58:36 -0500

    Simplify RotateMatrix

--- RotateMatrix.java
+++ RotateMatrix.java
@@ -1,36 +1,20 @@
 public class RotateMatrix {
 
 	public static boolean rotate(int[][] matrix) {
-		int length = matrix.length - 1;
+		int len = matrix.length - 1;
 
-		// Need <= for the inner rotations
-		for (int i = 0; i <= length / 2; i++) {
-			for (int j = i; j < length - i; j++) {
-				// Top left - coord 1
-				int p1 = matrix[i][j];
-
-				// Top right - coord 2
-				int p2 = matrix[j][length - i];
-
-				// Bottom right - coord 3
-				int p3 = matrix[length - i][length - j];
-
-				// Bottom left - coord 4
-				int p4 = matrix[length - j][i];
-
-				// Coord 2 gets coord 1
-				matrix[j][length - i] = p1;
-
-				// Coord 3 gets coord 2
-				matrix[length - i][length - j] = p2;
-
-				// Coord 4 gets coord 3
-				matrix[length - j][i] = p3;
-
-				// Coord 1 gets coord 4
-				matrix[i][j] = p4;
+		// Need less than or equals because we have to
+		// subtract 1 since arrays start from 0
+		for (int i = 0; i <= len / 2; i++) {
+			for (int j = i; j < len - i; j++) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[len - j][i];
+				matrix[len - j][i] = matrix[len - i][len - j];
+				matrix[len - i][len - j] = matrix[j][len - i];
+				matrix[j][len - i] = temp;
 			}
 		}
+
 		return true;
 	}
 
