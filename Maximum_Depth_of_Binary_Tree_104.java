eef3e38b3cec481c83ad7ce9fac7dbc9eaf031fc
Sean Prashad <S.Prashad@outlook.com>
2019-05-18 21:26:33 -0400

    Add Maximum_Depth_of_Binary_Tree_104.java

---
+++ Maximum_Depth_of_Binary_Tree_104.java
@@ -0,0 +1,12 @@
+class Maximum_Depth_of_Binary_Tree_104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
