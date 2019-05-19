089618824dfffabafe1f95cbac60cf4fcbf0bc8d
Sean Prashad <S.Prashad@outlook.com>
2019-05-18 21:48:48 -0400

    Add Move_Zeroes_283.java

---
+++ Move_Zeroes_283.java
@@ -0,0 +1,19 @@
+public class Move_Zeroes_283 {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int curr = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[curr++] = nums[i];
+            }
+        }
+
+        for (int j = curr; j < nums.length; j++) {
+            nums[j] = 0;
+        }
+    }
+}
