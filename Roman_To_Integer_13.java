0dcd9984840704601e9a19fb719a9a78462964ae
Sean Prashad <S.Prashad@outlook.com>
2019-06-17 15:33:31 -0400

    Add Roman_To_Integer_13.java

---
+++ Roman_To_Integer_13.java
@@ -0,0 +1,64 @@
+public class Roman_To_Integer_13 {
+    public static int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int[] nums = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+            case 'M':
+                nums[i] = 1000;
+                break;
+            case 'D':
+                nums[i] = 500;
+                break;
+            case 'C':
+                nums[i] = 100;
+                break;
+            case 'L':
+                nums[i] = 50;
+                break;
+            case 'X':
+                nums[i] = 10;
+                break;
+            case 'V':
+                nums[i] = 5;
+                break;
+            case 'I':
+                nums[i] = 1;
+                break;
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
+                sum -= nums[i];
+            } else {
+                sum += nums[i];
+            }
+        }
+
+        return sum + nums[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+        String exampleOne = "III";
+        System.out.println(romanToInt(exampleOne));
+
+        String exampleTwo = "IV";
+        System.out.println(romanToInt(exampleTwo));
+
+        String exampleThree = "IX";
+        System.out.println(romanToInt(exampleThree));
+
+        String exampleFour = "LVIII";
+        System.out.println(romanToInt(exampleFour));
+
+        String exampleFive = "MCMXCIV";
+        System.out.println(romanToInt(exampleFive));
+
+        return;
+    }
+}
