f210795a7edf7f138009919f70e6d8c2b08d95c3
Sean Prashad <S.Prashad@outlook.com>
2019-06-18 13:14:38 -0400

    Add Pascale_Triangle_118.java

---
+++ Pascale_Triangle_118.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pascale_Triangle_118 {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+
+            for (int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
+            }
+
+            result.add(new ArrayList<Integer>(row));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        List<List<Integer>> result = generate(n);
+
+        for (List<Integer> l : result) {
+            System.out.println(l);
+        }
+        return;
+    }
+}
