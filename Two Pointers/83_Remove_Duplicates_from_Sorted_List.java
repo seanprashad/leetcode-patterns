eeb383af995f5509ff086ff03de0c67baf618f5f
Sean Prashad <S.Prashad@outlook.com>
2019-07-23 12:35:35 -0400

    Add 83_Remove_Duplicates_from_Sorted_List.java

---
+++ Two Pointers/83_Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,30 @@
+/*
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+*/
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode curr = head, fast = head.next;
+
+        while (curr != null) {
+            while (fast != null && curr.val == fast.val) {
+                fast = fast.next;
+            }
+
+            curr.next = fast;
+            curr = fast;
+        }
+
+        return head;
+    }
+}
