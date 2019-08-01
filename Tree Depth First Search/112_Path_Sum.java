cf9dc5b7e53e362890dc2c28fafacb15f2c155ce
Sean Prashad <S.Prashad@outlook.com>
2019-07-31 23:33:47 -0400

    Update 112_Path_Sum.java

--- Tree Depth First Search/112_Path_Sum.java
+++ Tree Depth First Search/112_Path_Sum.java
@@ -3,11 +3,9 @@ class Solution {
         if (root == null) {
             return false;
         }
-
         if (root.left == null && root.right == null && sum - root.val == 0) {
             return true;
         }
-
         return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 }
