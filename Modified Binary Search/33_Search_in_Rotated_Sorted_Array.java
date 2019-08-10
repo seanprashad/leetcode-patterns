a3713daba2e533513b4a37cd9df08ea2aeb41bc5
Sean Prashad <S.Prashad@outlook.com>
2019-08-10 19:54:52 -0400

    Add 33_Search_in_Rotated_Sorted_Array.java

---
+++ Modified Binary Search/33_Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[start]) {
+                if (target <= nums[mid] && target >= nums[start]) {
+                    end = mid;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
