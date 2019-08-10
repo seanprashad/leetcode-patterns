5b67164419ed89289fa0da52c4b0ef38bb7c4dac
Sean Prashad <S.Prashad@outlook.com>
2019-08-10 17:36:32 -0400

    Update 162_Find_Peak_Element.java

--- Modified Binary Search/162_Find_Peak_Element.java
+++ Modified Binary Search/162_Find_Peak_Element.java
@@ -1,17 +1,17 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int l = 0, r = nums.length - 1;
+        int start = 0, end = nums.length - 1;
 
-        while (l < r) {
-            int m = l + (r - l) / 2;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
 
-            if (nums[m] < nums[m + 1]) {
-                l = m + 1;
+            if (nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
             } else {
-                r = m;
+                end = mid;
             }
         }
 
-        return l;
+        return start;
     }
 }
