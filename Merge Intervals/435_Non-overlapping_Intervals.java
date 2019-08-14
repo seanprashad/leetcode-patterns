fee07e9912044975bffe3811b81ab5ce3006c992
Sean Prashad <S.Prashad@outlook.com>
2019-08-14 15:13:32 -0400

    Update 435_Non-overlapping_Intervals.java

--- Merge Intervals/435_Non-overlapping_Intervals.java
+++ Merge Intervals/435_Non-overlapping_Intervals.java
@@ -1,28 +1,26 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        if (intervals == null || intervals.length <= 1) {
+        if (intervals == null || intervals.length == 0) {
             return 0;
         }
 
-        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+        Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
 
-        int[] prev = intervals[0];
-        int count = 0;
+        int result = 0;
+        int[] prevInterval = intervals[0];
 
         for (int i = 1; i < intervals.length; i++) {
-            int[] curr = intervals[i];
-
-            if (prev[1] <= curr[0]) {
-                prev = curr;
+            if (prevInterval[1] <= intervals[i][0]) {
+                prevInterval = intervals[i];
                 continue;
             }
 
-            ++count;
-            if (prev[1] > curr[1]) {
-                prev = curr;
+            ++result;
+            if (intervals[i][1] < prevInterval[1]) {
+                prevInterval = intervals[i];
             }
         }
 
-        return count;
+        return result;
     }
 }
