eb14adf6384a0bb34ac56db08988c75e00c19913
Sean Prashad <S.Prashad@outlook.com>
2019-08-21 21:36:42 -0400

    Add 53_Maximum_Subarray.java

---
+++ Dynamic Programming/53_Maximum_Subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        int max = dp[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
