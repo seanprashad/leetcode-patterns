3670680bd9906f8b07c7f848212e7d860cf405f6
Sean Prashad <S.Prashad@outlook.com>
2019-08-22 21:53:59 -0400

    Add 300_Longest_Increasing_Subsequence.java

---
+++ Dynamic Programming/300_Longest_Increasing_Subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int max = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
