fe7397efff246dbf5286459bda8a87fd1f46906f
Sean Prashad <S.Prashad@outlook.com>
2019-08-24 16:02:23 -0400

    Update 322_Coin_Change.java

--- Dynamic Programming/322_Coin_Change.java
+++ Dynamic Programming/322_Coin_Change.java
@@ -1,17 +1,18 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
         dp[0] = 0;
-        
-        for (int coin : coins) {
-            for (int i = 1; i <= amount; i++) {
-                if (i >= coin) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+
+        for (int sum = 1; sum <= amount; sum++) {
+            for (int coin : coins) {
+                if (coin <= sum && dp[sum - coin] < dp[sum]) {
+                    dp[sum] = dp[sum - coin] + 1;
                 }
             }
         }
-        
-        return dp[amount] == amount + 1 ? -1 : dp[amount];
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
