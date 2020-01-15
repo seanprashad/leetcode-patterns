7b8e566ea8f6e6b49b9e3cca6495cb449b12f6b2
Sean Prashad <S.Prashad@outlook.com>
2020-01-15 16:37:11 -0500

    Update 287_Find_the_Duplicate_Number.java

--- Arrays/287_Find_the_Duplicate_Number.java
+++ Arrays/287_Find_the_Duplicate_Number.java
@@ -1,23 +1,19 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        if (nums == null || nums.length <= 1) {
-            return -1;
+        int tortoise = nums[0], hare = nums[0];
+
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        tortoise = nums[0];
+
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
         }
 
-        int slow = nums[0], fast = nums[nums[0]];
-
-        while (nums[slow] != nums[fast]) {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        }
-
-        fast = 0;
-
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-
-        return slow;
+        return tortoise;
     }
 }
