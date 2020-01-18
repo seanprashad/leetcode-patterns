760174c6044993f7059acb82e3c27541aec52af9
Sean Prashad <S.Prashad@outlook.com>
2020-01-17 23:20:18 -0500

    Update 42_Trapping_Rain_Water.java

--- Two Pointers/42_Trapping_Rain_Water.java
+++ Two Pointers/42_Trapping_Rain_Water.java
@@ -4,17 +4,20 @@ class Solution {
             return 0;
         }
 
-        int sum = 0, left = 0, right = height.length - 1;
-        int leftMax = Integer.MIN_VALUE, rightMax = Integer.MIN_VALUE;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int sum = 0;
 
         while (left < right) {
             leftMax = Math.max(leftMax, height[left]);
             rightMax = Math.max(rightMax, height[right]);
 
             if (leftMax < rightMax) {
-                sum += leftMax - height[left++];
+                sum += leftMax - height[left];
+                ++left;
             } else {
-                sum += rightMax - height[right--];
+                sum += rightMax - height[right];
+                --right;
             }
         }
 
