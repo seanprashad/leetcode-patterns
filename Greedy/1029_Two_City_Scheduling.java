c6ce84aa33fd7d84f0d6cb3afe2f1666f966d0ca
Sean Prashad <S.Prashad@outlook.com>
2020-01-28 13:30:47 -0500

    Add 1029_Two_City_Scheduling.java

---
+++ Greedy/1029_Two_City_Scheduling.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, (a, b) -> {
+            return (a[0] - a[1]) - (b[0] - b[1]);
+        });
+
+        int totalCost = 0;
+
+        for (int i = 0; i < costs.length; i++) {
+            if (i < costs.length / 2) {
+                totalCost += costs[i][0];
+            } else {
+                totalCost += costs[i][1];
+            }
+        }
+
+        return totalCost;
+    }
+}
