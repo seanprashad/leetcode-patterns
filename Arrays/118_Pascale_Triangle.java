c266db71d2227fdc8429a1b6e431ddc84f6b7826
Sean Prashad <S.Prashad@outlook.com>
2020-01-30 12:59:11 -0500

    Update 118_Pascale_Triangle.java

--- Arrays/118_Pascale_Triangle.java
+++ Arrays/118_Pascale_Triangle.java
@@ -1,18 +1,29 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> row = new ArrayList<>();
-
-        for (int i = 0; i < numRows; i++) {
-            row.add(0, 1);
-
-            for (int j = 1; j < row.size() - 1; j++) {
-                row.set(j, row.get(j) + row.get(j + 1));
-            }
-
-            result.add(new ArrayList<Integer>(row));
+        if (numRows <= 0) {
+            return Collections.emptyList();
         }
 
-        return result;
+        List<List<Integer>> triangle = new ArrayList<>();
+        List<Integer> firstRow = new ArrayList<>();
+
+        firstRow.add(1);
+        triangle.add(firstRow);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = triangle.get(i - 1);
+            List<Integer> newRow = new ArrayList<>();
+
+            newRow.add(1);
+
+            for (int j = 1; j < i; j++) {
+                newRow.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+
+            newRow.add(1);
+            triangle.add(newRow);
+        }
+
+        return triangle;
     }
 }
