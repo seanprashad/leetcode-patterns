91abaa284d803c489f6c10d0868a750edf8f06e3
Sean Prashad <S.Prashad@outlook.com>
2020-01-29 22:34:49 -0500

    Update 33_Search_in_Rotated_Sorted_Array.java

--- Modified Binary Search/33_Search_in_Rotated_Sorted_Array.java
+++ Modified Binary Search/33_Search_in_Rotated_Sorted_Array.java
@@ -4,25 +4,39 @@ class Solution {
             return -1;
         }
 
-        int start = 0, end = nums.length - 1;
+        int left = 0, right = nums.length - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        while (left < right) {
+            int mid = left + ((right - left) / 2);
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        int start = left;
+        left = 0;
+        right = nums.length - 1;
+
+        if (target >= nums[start] && target <= nums[right]) {
+            left = start;
+        } else {
+            right = start;
+        }
+
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
 
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[start] <= nums[mid]) {
-                if (nums[start] <= target && target <= nums[mid]) {
-                    end = mid;
-                } else {
-                    start = mid + 1;
-                }
+            }
+
+            if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                if (nums[mid] <= target && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid;
-                }
+                right = mid - 1;
             }
         }
 
