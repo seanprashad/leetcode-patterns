5aeab51ab3ed1ac5ac0d91b5df6791dad3c41b0c
Sean Prashad <S.Prashad@outlook.com>
2020-02-23 01:15:23 -0500

    Update 42_Trapping_Rain_Water.java

--- Two Pointers/42_Trapping_Rain_Water.java
+++ Two Pointers/42_Trapping_Rain_Water.java
@@ -5,21 +5,21 @@ class Solution {
         }
 
         int water = 0;
-        int[] leftMax = new int[height.length], rightMax = new int[height.length];
+        int leftMax = height[0], rightTallest = height[height.length - 1];
+        int[] leftMaxes = new int[height.length];
 
-        leftMax[0] = height[0];
-        rightMax[rightMax.length - 1] = height[height.length - 1];
-
-        for (int i = 1; i < height.length; i++) {
-            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        for (int i = 0; i < height.length; i++) {
+            leftMax = Math.max(leftMax, height[i]);
+            leftMaxes[i] = leftMax;
         }
 
-        for (int i = height.length - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
-        }
+        for (int i = height.length - 1; i >= 0; i--) {
+            rightTallest = Math.max(rightTallest, height[i]);
 
-        for (int i = 1; i < height.length - 1; i++) {
-            water += Math.min(leftMax[i], rightMax[i]) - height[i];
+            int leftTallest = Math.min(rightTallest, leftMaxes[i]);
+            if (leftTallest > height[i]) {
+                water += leftTallest - height[i];
+            }
         }
 
         return water;
