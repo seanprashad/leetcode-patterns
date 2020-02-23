836ba1d8197f9014b937a0eda1337488f3642316
Sean Prashad <S.Prashad@outlook.com>
2020-02-23 13:58:53 -0500

    Update 295_Find_Median_from_Data_Stream.java

--- Two Heaps/295_Find_Median_from_Data_Stream.java
+++ Two Heaps/295_Find_Median_from_Data_Stream.java
@@ -1,22 +1,30 @@
 class MedianFinder {
-    private PriorityQueue<Integer> minHeap;
-    private PriorityQueue<Integer> maxHeap;
+    private PriorityQueue<Integer> minHeap, maxHeap;
+    boolean isEven;
 
     public MedianFinder() {
         minHeap = new PriorityQueue<>();
-        maxHeap = new PriorityQueue<>((a, b) -> b.compareTo(a));
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        isEven = true;
     }
 
     public void addNum(int num) {
-        maxHeap.offer(num);
-        minHeap.offer(maxHeap.poll());
-
-        if (maxHeap.size() < minHeap.size()) {
+        if (isEven) {
+            maxHeap.offer(num);
+            minHeap.offer(maxHeap.poll());
+        } else {
+            minHeap.offer(num);
             maxHeap.offer(minHeap.poll());
         }
+
+        isEven = !isEven;
     }
 
     public double findMedian() {
-        return minHeap.size() == maxHeap.size() ? (double) (minHeap.peek() + maxHeap.peek()) / 2.0 : maxHeap.peek();
+        if (isEven) {
+            return (double) (maxHeap.peek() + minHeap.peek()) / 2;
+        } else {
+            return minHeap.peek();
+        }
     }
 }
