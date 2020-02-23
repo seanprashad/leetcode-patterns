78e72a602c0d7329cccaf5b78ebc6d65206d454a
Sean Prashad <S.Prashad@outlook.com>
2020-02-22 23:28:06 -0500

    Update 215_Kth_Largest_Element_in_an_Array.java

--- Top K Elements/215_Kth_Largest_Element_in_an_Array.java
+++ Top K Elements/215_Kth_Largest_Element_in_an_Array.java
@@ -1,34 +1,43 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int start = 0, end = nums.length - 1, resultIdx = nums.length - k;
-        
+        int start = 0, end = nums.length - 1, target = nums.length - k;
+
         while (start < end) {
-            int pivotIdx = partition(nums, start, end);
-            
-            if (pivotIdx < resultIdx) { start = pivotIdx + 1; }
-            else if (pivotIdx > resultIdx) { end = pivotIdx - 1; }
-            else { return nums[pivotIdx]; }
+            int pivot = partition(nums, start, end);
+
+            if (pivot == target) {
+                return nums[pivot];
+            } else if (pivot < target) {
+                start = pivot + 1;
+            } else {
+                end = pivot - 1;
+            }
         }
-        
+
         return nums[start];
     }
-    
+
     private int partition(int[] nums, int start, int end) {
-        int pivot = end;
-        
+        int pivot = start;
+
         while (start <= end) {
-            while (start <= end && nums[start] <= nums[pivot]) { ++start; }
-            while (start <= end && nums[end] > nums[pivot]) { --end; }
-            
-            if (start > end) { break; }
-            
+            while (start <= end && nums[start] <= nums[pivot]) {
+                start++;
+            }
+            while (start <= end && nums[end] > nums[pivot]) {
+                end--;
+            }
+
+            if (start > end)
+                break;
+
             swap(nums, start, end);
         }
-        
-        swap(nums, pivot, end);
+
+        swap(nums, end, pivot);
         return end;
     }
-    
+
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
