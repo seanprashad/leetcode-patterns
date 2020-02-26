5e774e1c5a99d936e2418d1026f4288bdf15513f
Sean Prashad <S.Prashad@outlook.com>
2020-02-25 23:31:23 -0500

    Update 239_Sliding_Window_Maximum.java

--- Sliding Window/239_Sliding_Window_Maximum.java
+++ Sliding Window/239_Sliding_Window_Maximum.java
@@ -1,34 +1,25 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return new int[] {};
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return new int[0];
         }
 
         int[] result = new int[nums.length - k + 1];
+        Deque<Integer> dq = new LinkedList<>();
 
-        Deque<Integer> dq = new ArrayDeque<>();
-        int idx = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            // Remove entries outside of the current window
-            while (!dq.isEmpty() && dq.peek() < i - k + 1) {
+        for (int idx = 0; idx < nums.length; idx++) {
+            if (!dq.isEmpty() && dq.peek() < idx - k + 1) {
                 dq.pollFirst();
             }
 
-            // Remove entries that are smaller than the current indexes value
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+            while (!dq.isEmpty() && nums[idx] >= nums[dq.peekLast()]) {
                 dq.pollLast();
             }
 
-            dq.offer(i);
+            dq.offer(idx);
 
-            // Only begin adding the highest values when our sliding window
-            // is greater than or equal to k elements
-            //
-            // This allows us to sample the max of the first k elements
-            // before deciding if we should add it to the result
-            if (i >= k - 1) {
-                result[idx++] = nums[dq.peek()];
+            if (idx - k + 1 >= 0) {
+                result[idx - k + 1] = nums[dq.peekFirst()];
             }
         }
 
