cbd7c1562ee19a9566afc0f239299b62fd77ce2c
Sean Prashad <S.Prashad@outlook.com>
2020-03-18 16:57:35 -0400

    Update 22_Generate_Parentheses.java

--- Backtracking/22_Generate_Parentheses.java
+++ Backtracking/22_Generate_Parentheses.java
@@ -1,30 +1,27 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        if (n <= 0) {
-            return Collections.emptyList();
-        }
-
         List<String> result = new ArrayList<>();
-        dfs(n, 0, 0, new StringBuilder(), result);
+
+        helper(n, 0, 0, new StringBuilder(), result);
         return result;
     }
 
-    private void dfs(int n, int open, int close, StringBuilder sb, List<String> result) {
-        if (open == n && close == n) {
-            result.add(sb.toString());
+    private void helper(int n, int open, int close, StringBuilder temp, List<String> result) {
+        if (temp.length() == n * 2) {
+            result.add(temp.toString());
             return;
         }
 
         if (open < n) {
-            sb.append('(');
-            dfs(n, open + 1, close, sb, result);
-            sb.deleteCharAt(sb.length() - 1);
+            temp.append("(");
+            helper(n, open + 1, close, temp, result);
+            temp.deleteCharAt(temp.length() - 1);
         }
 
         if (close < open) {
-            sb.append(')');
-            dfs(n, open, close + 1, sb, result);
-            sb.deleteCharAt(sb.length() - 1);
+            temp.append(")");
+            helper(n, open, close + 1, temp, result);
+            temp.deleteCharAt(temp.length() - 1);
         }
     }
 }
