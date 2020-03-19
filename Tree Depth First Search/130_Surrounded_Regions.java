f644506bd47def931d0c57a00ce4a1f1085fd14a
Sean Prashad <S.Prashad@outlook.com>
2020-03-18 22:50:19 -0400

    Add 130_Surrounded_Regions.java

---
+++ Tree Depth First Search/130_Surrounded_Regions.java
@@ -0,0 +1,51 @@
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][n - 1] == 'O') {
+                dfs(board, i, n - 1);
+            }
+        }
+
+        for (int j = 1; j < n - 1; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+            if (board[m - 1][j] == 'O') {
+                dfs(board, m - 1, j);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '*') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int x, int y) {
+        if (x < 0 || y < 0 || x >= board.length || y >= board[x].length || board[x][y] != 'O') {
+            return;
+        }
+
+        board[x][y] = '*';
+
+        dfs(board, x - 1, y);
+        dfs(board, x + 1, y);
+        dfs(board, x, y - 1);
+        dfs(board, x, y + 1);
+    }
+}
