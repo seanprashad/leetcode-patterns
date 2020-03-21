d4c2009147602ec61980ecb7c81f870440653ace
Sean Prashad <S.Prashad@outlook.com>
2020-03-20 20:38:33 -0400

    Update 17_Letter_Combinations_of_a_Phone_Number.java

--- Backtracking/17_Letter_Combinations_of_a_Phone_Number.java
+++ Backtracking/17_Letter_Combinations_of_a_Phone_Number.java
@@ -1,28 +1,43 @@
 class Solution {
-    private static final String[] KEYS = new String[] { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv",
-            "wxyz" };
-
     public List<String> letterCombinations(String digits) {
         if (digits == null || digits.length() == 0) {
             return Collections.emptyList();
         }
-
+        Map<Character, String> keypad = buildKeypad();
         List<String> result = new ArrayList<>();
-        dfs(digits, 0, new StringBuilder(), result);
+
+        helper(digits, 0, new StringBuilder(), result, keypad);
         return result;
     }
 
-    private void dfs(String digits, int offset, StringBuilder sb, List<String> result) {
-        if (offset >= digits.length()) {
+    private void helper(String digits, int idx, StringBuilder sb, List<String> result, Map<Character, String> keypad) {
+        if (sb.length() == digits.length()) {
             result.add(sb.toString());
             return;
         }
 
-        String letters = KEYS[digits.charAt(offset) - '0'];
+        String letters = keypad.get(digits.charAt(idx));
+
         for (int i = 0; i < letters.length(); i++) {
             sb.append(letters.charAt(i));
-            dfs(digits, offset + 1, sb, result);
+            helper(digits, idx + 1, sb, result, keypad);
             sb.deleteCharAt(sb.length() - 1);
         }
+    }
+
+    private Map<Character, String> buildKeypad() {
+        Map<Character, String> keypad = new HashMap<>();
+
+        keypad.put('1', "");
+        keypad.put('2', "abc");
+        keypad.put('3', "def");
+        keypad.put('4', "ghi");
+        keypad.put('5', "jkl");
+        keypad.put('6', "mno");
+        keypad.put('7', "pqrs");
+        keypad.put('8', "tuv");
+        keypad.put('9', "wxyz");
+
+        return keypad;
     }
 }
