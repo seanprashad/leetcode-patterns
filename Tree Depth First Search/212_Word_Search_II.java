652c0c348785866a3f613e48a4f18347e5529782
Sean Prashad <S.Prashad@outlook.com>
2020-03-20 22:37:04 -0400

    Update 212_Word_Search_II.java

--- Tree Depth First Search/212_Word_Search_II.java
+++ Tree Depth First Search/212_Word_Search_II.java
@@ -1,59 +1,66 @@
-class TrieNode {
-    TrieNode[] children = new TrieNode[26];
-    String Word;
-}
-
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
-        Set<String> result = new HashSet<>();
+        List<String> result = new ArrayList<>();
         TrieNode root = buildTrie(words);
-        
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 dfs(board, i, j, root, result);
             }
         }
-        
-        return new ArrayList<>(result);
+
+        return result;
     }
-    
-    private void dfs(char[][] board, int x, int y, TrieNode root, Set<String> result) {
-        if (x < 0 || y < 0 || x >= board.length || y >= board[x].length || board[x][y] == '*' || root.children[board[x][y] - 'a'] == null) { return; }
-        
-        char currChar = board[x][y];
-        root = root.children[currChar - 'a'];
-        
-        if (root.Word != null) {
-            result.add(root.Word);
+
+    private void dfs(char[][] board, int x, int y, TrieNode root, List<String> result) {
+        if (root.word != null) {
+            result.add(root.word);
+            root.word = null;
+            return;
         }
-        
-        board[x][y] = '*';
-        
-        dfs(board, x - 1, y, root, result);
+
+        if (x < 0 || y < 0 || x >= board.length || y >= board[x].length || board[x][y] == '@'
+                || root.children.get(board[x][y]) == null) {
+            return;
+        }
+
+        char c = board[x][y];
+        board[x][y] = '@';
+
+        root = root.children.get(c);
+
         dfs(board, x + 1, y, root, result);
-        dfs(board, x, y - 1, root, result);
+        dfs(board, x - 1, y, root, result);
         dfs(board, x, y + 1, root, result);
-            
-        board[x][y] = currChar;
+        dfs(board, x, y - 1, root, result);
+
+        board[x][y] = c;
     }
-    
+
     private TrieNode buildTrie(String[] words) {
-        if (words == null) { return null; }
-        
         TrieNode root = new TrieNode();
-        for (String word : words) {
-            TrieNode curr = root;
-            for (char c : word.toCharArray()) {
-                int idx = c - 'a';
-                if (curr.children[idx] == null) {
-                    curr.children[idx] = new TrieNode();
-                }
-                curr = curr.children[idx];
+
+        for (String w : words) {
+            TrieNode runner = root;
+
+            for (char c : w.toCharArray()) {
+                runner.children.putIfAbsent(c, new TrieNode());
+                runner = runner.children.get(c);
             }
-            
-            curr.Word = word;
+
+            runner.word = w;
         }
-        
+
         return root;
+    }
+
+    private class TrieNode {
+        private Map<Character, TrieNode> children;
+        private String word;
+
+        public TrieNode() {
+            children = new HashMap<>();
+            word = null;
+        }
     }
 }
