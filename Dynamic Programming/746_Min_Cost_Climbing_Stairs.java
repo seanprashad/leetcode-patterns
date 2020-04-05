f3f9204abb3a172ea3339f963cf34f89c220ab42
Sean Prashad <S.Prashad@outlook.com>
2020-04-04 20:00:40 -0400

    Update 746_Min_Cost_Climbing_Stairs.java

--- Dynamic Programming/746_Min_Cost_Climbing_Stairs.java
+++ Dynamic Programming/746_Min_Cost_Climbing_Stairs.java
@@ -1,10 +1,7 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        if (cost == null || cost.length == 0) {
-            return 0;
-        }
-
         int[] dp = new int[cost.length];
+
         dp[0] = cost[0];
         dp[1] = cost[1];
 
@@ -12,6 +9,6 @@ class Solution {
             dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
         }
 
-        return Math.min(dp[dp.length - 1], dp[dp.length - 2]);
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
     }
 }
