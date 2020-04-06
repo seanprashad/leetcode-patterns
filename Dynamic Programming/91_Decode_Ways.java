3de30de108a22c67fef72ccd62365ed8ed186491
Sean Prashad <S.Prashad@outlook.com>
2020-04-05 21:47:07 -0400

    Update 91_Decode_Ways.java

--- Dynamic Programming/91_Decode_Ways.java
+++ Dynamic Programming/91_Decode_Ways.java
@@ -1,18 +1,26 @@
 class Solution {
     public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
         int[] dp = new int[s.length() + 1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+
+        if (s.charAt(0) != '0') {
+            dp[0] = 1;
+        }
+        if (dp[0] == 1) {
+            dp[1] = 1;
+        }
 
         for (int i = 2; i <= s.length(); i++) {
-            int oneDigit = Integer.valueOf(s.substring(i - 1, i));
-            int twoDigits = Integer.valueOf(s.substring(i - 2, i));
-
-            if (1 <= oneDigit && oneDigit <= 9) {
+            if (s.charAt(i - 1) != '0') {
                 dp[i] += dp[i - 1];
             }
 
-            if (10 <= twoDigits && twoDigits <= 26) {
+            int twoDigits = Integer.valueOf(s.substring(i - 2, i));
+
+            if (twoDigits >= 10 && twoDigits <= 26) {
                 dp[i] += dp[i - 2];
             }
         }
