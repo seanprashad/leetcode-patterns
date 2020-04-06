b366358a3fea056f9d06050b86e82026acf37720
Sean Prashad <S.Prashad@outlook.com>
2020-04-06 17:37:43 -0400

    Update 10_Regular_Expression_Matching.java

--- Dynamic Programming/10_Regular_Expression_Matching.java
+++ Dynamic Programming/10_Regular_Expression_Matching.java
@@ -1,30 +1,23 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m = s.length(), n = p.length();
-
-        boolean[][] dp = new boolean[m + 1][n + 1];
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
         dp[0][0] = true;
 
-        for (int j = 1; j <= n; j++) {
-            if (p.charAt(j - 1) == '*') {
-                dp[0][j] = dp[0][j - 2];
-            }
+        for (int j = 2; j <= p.length(); j++) {
+            dp[0][j] = p.charAt(j - 1) == '*' && dp[0][j - 2];
         }
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
                 if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else if (p.charAt(j - 1) == '*') {
-                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
-                        dp[i][j] = dp[i][j - 2];
-                    } else {
-                        dp[i][j] = dp[i][j - 2] || dp[i][j - 1] || dp[i - 1][j];
-                    }
+                    dp[i][j] = dp[i][j - 2]
+                            || (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') && dp[i - 1][j];
                 }
             }
         }
 
-        return dp[m][n];
+        return dp[s.length()][p.length()];
     }
 }
