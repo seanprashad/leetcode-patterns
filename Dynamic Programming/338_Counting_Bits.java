7be5ae7233984be93b7c5cfa9ac28423dfae650b
Sean Prashad <S.Prashad@outlook.com>
2020-04-14 00:47:20 -0400

    Update 338_Counting_Bits.java

--- Dynamic Programming/338_Counting_Bits.java
+++ Dynamic Programming/338_Counting_Bits.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] countBits(int num) {
-        int[] dp = new int[num + 1];
-        int offset = 1;
+        int[] result = new int[num + 1];
 
         for (int i = 1; i <= num; i++) {
-            if (offset * 2 == i) {
-                offset *= 2;
+            if (i % 2 == 0) {
+                result[i] = result[i >> 1];
+            } else {
+                result[i] = result[i - 1] + 1;
             }
-
-            dp[i] = dp[i - offset] + 1;
         }
 
-        return dp;
+        return result;
     }
 }
