a6ccd6a6fbd1ca45d820ae31fa8e11de2d2a36eb
Sean Prashad <S.Prashad@outlook.com>
2020-04-17 21:48:47 -0400

    Update 96_Unique_Binary_Search_Trees.java

--- Dynamic Programming/96_Unique_Binary_Search_Trees.java
+++ Dynamic Programming/96_Unique_Binary_Search_Trees.java
@@ -1,17 +1,14 @@
 class Solution {
     public int numTrees(int n) {
-        if (n < 0) {
-            return 0;
-        }
-
         int[] dp = new int[n + 1];
-        dp[0] = 1;
 
-        for (int i = 1; i <= n; i++) {
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 dp[i] += dp[j - 1] * dp[i - j];
             }
-
         }
 
         return dp[n];
