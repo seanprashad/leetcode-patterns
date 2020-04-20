65d83b8a1ab7e00af2ce5508942c312aa596eff3
Sean Prashad <S.Prashad@outlook.com>
2020-04-20 15:40:23 -0400

    Update 32_Longest_Valid_Parentheses.java

--- Dynamic Programming/32_Longest_Valid_Parentheses.java
+++ Dynamic Programming/32_Longest_Valid_Parentheses.java
@@ -1,24 +1,23 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        if (s == null || s.length() == 0) {
+        if (s == null || s.length() < 2) {
             return 0;
         }
 
+        int result = 0, leftCount = 0;
         int[] dp = new int[s.length()];
-        dp[0] = 0;
-        int leftBracketCount = 0, result = 0;
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                ++leftBracketCount;
-            } else if (leftBracketCount > 0) {
+                ++leftCount;
+            } else if (leftCount > 0) {
                 dp[i] = dp[i - 1] + 2;
 
-                if (i - dp[i] > 0) {
+                if (i - dp[i] >= 0) {
                     dp[i] += dp[i - dp[i]];
                 }
 
-                --leftBracketCount;
+                --leftCount;
                 result = Math.max(result, dp[i]);
             }
         }
