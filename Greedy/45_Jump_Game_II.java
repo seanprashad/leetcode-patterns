8b42f2126ad2968edd258b778f546c4c1e24303e
Sean Prashad <S.Prashad@outlook.com>
2020-04-21 18:30:37 -0400

    Update 45_Jump_Game_II.java

--- Greedy/45_Jump_Game_II.java
+++ Greedy/45_Jump_Game_II.java
@@ -1,20 +1,20 @@
 class Solution {
     public int jump(int[] nums) {
-        if (nums == null || nums.length == 0) {
+        if (nums == null || nums.length < 2) {
             return 0;
         }
 
-        int result = 0, currEnd = 0, currFarthest = 0;
+        int currFarthest = 0, currEnd = 0, jumps = 0;
 
-        for (int i = 0; i < nums.length - 1; i++) {
-            currFarthest = Math.max(currFarthest, i + nums[i]);
-
-            if (i == currEnd) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i > currEnd) {
                 currEnd = currFarthest;
-                ++result;
+                ++jumps;
             }
+
+            currFarthest = Math.max(currFarthest, i + nums[i]);
         }
 
-        return result;
+        return jumps;
     }
 }
