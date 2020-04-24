70862f9efb180657751bd956895cde2249faf9b8
Sean Prashad <S.Prashad@outlook.com>
2020-04-24 01:15:51 -0400

    Update 15_Three_Sum.java

--- Two Pointers/15_Three_Sum.java
+++ Two Pointers/15_Three_Sum.java
@@ -1,31 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums == null || nums.length < 3) {
+        if (nums == null || nums.length == 0) {
             return Collections.emptyList();
         }
 
-        Set<List<Integer>> result = new HashSet<>();
-
         Arrays.sort(nums);
 
+        List<List<Integer>> result = new ArrayList<>();
+
         for (int i = 0; i < nums.length - 2; i++) {
-            int j = i + 1, k = nums.length - 1;
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int j = i + 1, k = nums.length - 1;
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
 
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    j++;
-                } else {
-                    k--;
+                    if (sum == 0) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                        while (j < k && nums[j] == nums[j + 1]) {
+                            ++j;
+                        }
+                        while (j < k && nums[k] == nums[k - 1]) {
+                            --k;
+                        }
+
+                        ++j;
+                        --k;
+                    } else if (sum < 0) {
+                        ++j;
+                    } else {
+                        --k;
+                    }
                 }
             }
         }
 
-        return new ArrayList<>(result);
+        return result;
     }
 }
