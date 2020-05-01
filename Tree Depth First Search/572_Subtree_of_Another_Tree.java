a84fd934c21985e7e1b29e911040ac22df5a19fc
Sean Prashad <S.Prashad@outlook.com>
2020-05-01 00:13:59 -0400

    Update 572_Subtree_of_Another_Tree.java

--- Tree Depth First Search/572_Subtree_of_Another_Tree.java
+++ Tree Depth First Search/572_Subtree_of_Another_Tree.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isSubtree(TreeNode s, TreeNode t) {
-        if (s == null || t == null) {
-            return s == t;
+        if (s == null) {
+            return false;
         }
-        return isSameTree(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+        return helper(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
     }
 
-    private boolean isSameTree(TreeNode t1, TreeNode t2) {
-        if (t1 == null || t2 == null) {
-            return t1 == t2;
+    private boolean helper(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
         }
-
-        if (t1.val != t2.val) {
+        if (s == null || t == null) {
             return false;
         }
 
-        return isSameTree(t1.left, t2.left) && isSameTree(t1.right, t2.right);
+        if (s.val != t.val) {
+            return false;
+        }
+
+        return helper(s.left, t.left) && helper(s.right, t.right);
     }
 }
