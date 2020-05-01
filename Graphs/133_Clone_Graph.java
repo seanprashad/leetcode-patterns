0634abca2332feec07ef9635308194f4fb62eca7
Sean Prashad <S.Prashad@outlook.com>
2020-05-01 17:19:07 -0400

    Update 133_Clone_Graph.java

--- Graphs/133_Clone_Graph.java
+++ Graphs/133_Clone_Graph.java
@@ -1,28 +1,25 @@
 class Solution {
     public Node cloneGraph(Node node) {
+        Map<Node, Node> map = new HashMap<>();
+        return helper(node, map);
+    }
+
+    private Node helper(Node node, Map<Node, Node> visited) {
         if (node == null) {
             return null;
         }
 
-        Map<Node, Node> map = new HashMap<>();
-        map.put(node, new Node(node.val));
-
-        Queue<Node> q = new LinkedList<>();
-        q.offer(node);
-
-        while (!q.isEmpty()) {
-            Node n = q.poll();
-
-            for (Node neighbor : n.neighbors) {
-                if (!map.containsKey(neighbor)) {
-                    map.put(neighbor, new Node(neighbor.val));
-                    q.offer(neighbor);
-                }
-
-                map.get(n).neighbors.add(map.get(neighbor));
-            }
+        if (visited.containsKey(node)) {
+            return visited.get(node);
         }
 
-        return map.get(node);
+        Node cloned = new Node(node.val);
+        visited.put(node, cloned);
+
+        for (Node n : node.neighbors) {
+            cloned.neighbors.add(helper(n, visited));
+        }
+
+        return cloned;
     }
 }
