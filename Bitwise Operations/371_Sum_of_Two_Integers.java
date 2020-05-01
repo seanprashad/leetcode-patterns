3e202cf63221961a2df542e30bab5d03a4e9b59d
Sean Prashad <S.Prashad@outlook.com>
2020-05-01 15:51:34 -0400

    Update 371_Sum_of_Two_Integers.java

--- Bitwise Operations/371_Sum_of_Two_Integers.java
+++ Bitwise Operations/371_Sum_of_Two_Integers.java
@@ -2,7 +2,9 @@ class Solution {
     public int getSum(int a, int b) {
         while (b != 0) {
             int carry = a & b;
-            a ^= b;
+            int xor = a ^ b;
+
+            a = xor;
             b = carry << 1;
         }
 
