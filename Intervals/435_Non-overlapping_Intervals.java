3e259fdd8133d2e13b0dc6b4b7188afec497cbfa
Sean Prashad <S.Prashad@outlook.com>
2020-05-04 18:08:38 -0400

    Update 435_Non-overlapping_Intervals.java

--- Intervals/435_Non-overlapping_Intervals.java
+++ Intervals/435_Non-overlapping_Intervals.java
@@ -7,17 +7,14 @@ class Solution {
         Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
 
         int result = 0;
-        int[] prevInterval = intervals[0];
+        int[] prev = intervals[0];
 
         for (int i = 1; i < intervals.length; i++) {
-            if (prevInterval[1] <= intervals[i][0]) {
-                prevInterval = intervals[i];
-                continue;
-            }
-
-            ++result;
-            if (intervals[i][1] < prevInterval[1]) {
-                prevInterval = intervals[i];
+            if (intervals[i][0] < prev[1]) {
+                ++result;
+                prev[1] = Math.min(prev[1], intervals[i][1]);
+            } else {
+                prev = intervals[i];
             }
         }
 
