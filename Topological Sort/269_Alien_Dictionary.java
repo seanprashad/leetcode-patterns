5d9ca63faa0dc9182b4340090dbe0fb04d22ce10
Sean Prashad <S.Prashad@outlook.com>
2020-05-05 15:14:32 -0400

    Update 269_Alien_Dictionary.java

--- Topological Sort/269_Alien_Dictionary.java
+++ Topological Sort/269_Alien_Dictionary.java
@@ -1,59 +1,37 @@
 class Solution {
     public String alienOrder(String[] words) {
-        if (words == null || words.length == 0) {
-            return "";
-        }
-
-        int indegrees[] = new int[26];
-        Map<Character, List<Character>> graph = buildGraph(words, indegrees);
-
-        if (graph == null) {
-            return "";
-        }
-
-        return topologicalSort(graph, indegrees);
-    }
-
-    private Map<Character, List<Character>> buildGraph(String[] words, int[] indegrees) {
-        Map<Character, List<Character>> graph = new HashMap<>();
+        Map<Character, List<Character>> adjList = new HashMap<>();
+        Map<Character, Integer> inorderMap = new HashMap<>();
 
         for (String word : words) {
             for (char c : word.toCharArray()) {
-                graph.putIfAbsent(c, new ArrayList<>());
+                adjList.putIfAbsent(c, new ArrayList<>());
+                inorderMap.putIfAbsent(c, 0);
             }
         }
 
         for (int i = 0; i < words.length - 1; i++) {
-            String w1 = words[i];
-            String w2 = words[i + 1];
+            String word1 = words[i];
+            String word2 = words[i + 1];
 
-            int len = Math.min(w1.length(), w2.length());
+            if (word1.length() > word2.length() && word1.startsWith(word2)) {
+                return "";
+            }
 
-            for (int j = 0; j < len; j++) {
-                char c1 = w1.charAt(j);
-                char c2 = w2.charAt(j);
-
-                if (c1 != c2) {
-                    graph.get(c1).add(c2);
-                    indegrees[c2 - 'a']++;
+            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
+                if (word1.charAt(j) != word2.charAt(j)) {
+                    adjList.get(word1.charAt(j)).add(word2.charAt(j));
+                    inorderMap.put(word2.charAt(j), inorderMap.get(word2.charAt(j)) + 1);
                     break;
-                }
-
-                if (j == w2.length() - 1 && w1.length() > w2.length()) {
-                    return null;
                 }
             }
         }
 
-        return graph;
-    }
-
-    private String topologicalSort(Map<Character, List<Character>> graph, int[] indegrees) {
-        StringBuilder sb = new StringBuilder();
         Queue<Character> q = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
 
-        for (char c : graph.keySet()) {
-            if (indegrees[c - 'a'] == 0) {
+        for (char c : inorderMap.keySet()) {
+            if (inorderMap.get(c) == 0) {
                 q.offer(c);
             }
         }
@@ -62,15 +40,17 @@ class Solution {
             char c = q.poll();
             sb.append(c);
 
-            for (char neighbour : graph.get(c)) {
-                indegrees[neighbour - 'a']--;
-
-                if (indegrees[neighbour - 'a'] == 0) {
+            for (char neighbour : adjList.get(c)) {
+                inorderMap.put(neighbour, inorderMap.get(neighbour) - 1);
+                if (inorderMap.get(neighbour) == 0) {
                     q.offer(neighbour);
                 }
             }
         }
 
-        return sb.length() == graph.size() ? sb.toString() : "";
+        if (sb.length() < inorderMap.size()) {
+            return "";
+        }
+        return sb.toString();
     }
 }
