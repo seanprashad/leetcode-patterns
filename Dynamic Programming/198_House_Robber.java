2779395e7d3a6d9bfcb49513f5d6c48cd8e2ef47
Sean Prashad <S.Prashad@outlook.com>
2020-05-04 23:14:52 -0400

    Update 198_House_Robber.java

--- Dynamic Programming/198_House_Robber.java
+++ Dynamic Programming/198_House_Robber.java
@@ -10,14 +10,14 @@ class Solution {
             return Math.max(nums[0], nums[1]);
         }
 
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        int prevHouse = 0, prevTwoHouses = 0, currHouse = 0;
 
-        for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            currHouse = Math.max(prevHouse, prevTwoHouses + nums[i]);
+            prevTwoHouses = prevHouse;
+            prevHouse = currHouse;
         }
 
-        return dp[nums.length - 1];
+        return currHouse;
     }
 }
