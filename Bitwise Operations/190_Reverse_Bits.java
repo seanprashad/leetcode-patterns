c0e52d438c594672a0f850f0a6f6afcbbff269c9
Sean Prashad <S.Prashad@outlook.com>
2020-05-07 17:21:30 -0400

    Update 190_Reverse_Bits.java

--- Bitwise Operations/190_Reverse_Bits.java
+++ Bitwise Operations/190_Reverse_Bits.java
@@ -3,12 +3,9 @@ public class Solution {
         int result = 0;
 
         for (int i = 0; i < 32; i++) {
+            result <<= 1;
             result += n & 1;
-            n >>>= 1;
-
-            if (i < 31) {
-                result <<= 1;
-            }
+            n >>= 1;
         }
 
         return result;
