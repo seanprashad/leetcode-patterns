a045b06902c010573110544ff2867bbd9e193985
Sean Prashad <S.Prashad@outlook.com>
2020-05-06 20:54:11 -0400

    Update 55_Jump_Game.java

--- Greedy/55_Jump_Game.java
+++ Greedy/55_Jump_Game.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxJump = 0, currJump = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            maxJump = Math.max(maxJump, i + nums[i]);
-
-            if (i == currJump) {
-                currJump = Math.max(currJump, maxJump);
-
-                if (currJump >= nums.length - 1) {
-                    return true;
-                }
-            }
+        if (nums == null || nums.length == 0) {
+            return false;
         }
 
-        return false;
+        int reachable = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (reachable < i) {
+                return false;
+            }
+            reachable = Math.max(reachable, i + nums[i]);
+        }
+
+        return true;
     }
 }
