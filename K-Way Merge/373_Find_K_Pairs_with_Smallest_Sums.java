0498f17200541124cfed16c0e998ec86d2b25f80
Sean Prashad <S.Prashad@outlook.com>
2021-01-29 23:54:15 -0500

    Update 373_Find_K_Pairs_with_Smallest_Sums.java

--- K-Way Merge/373_Find_K_Pairs_with_Smallest_Sums.java
+++ K-Way Merge/373_Find_K_Pairs_with_Smallest_Sums.java
@@ -1,28 +1,24 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        if (nums1.length == 0 || nums2.length == 0 || k == 0) {
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) {
             return Collections.emptyList();
         }
 
         List<List<Integer>> result = new ArrayList<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] + a[1]) - (b[0] + b[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> (p1[0] + p1[1]) - (p2[0] + p2[1]));
 
         for (int i = 0; i < nums1.length; i++) {
             pq.offer(new int[] { nums1[i], nums2[0], 0 });
         }
 
         for (int i = 0; i < Math.min(nums1.length * nums2.length, k); i++) {
-            int[] curr = pq.poll();
+            int[] pair = pq.poll();
 
-            List<Integer> temp = new ArrayList<>();
-            temp.add(curr[0]);
-            temp.add(curr[1]);
+            result.add(Arrays.asList(pair[0], pair[1]));
 
-            result.add(temp);
-
-            if (curr[2] < nums2.length - 1) {
-                int idx = curr[2] + 1;
-                pq.offer(new int[] { curr[0], nums2[idx], idx });
+            if (pair[2] < nums2.length - 1) {
+                int nextIdx = pair[2] + 1;
+                pq.offer(new int[] { pair[0], nums2[nextIdx], nextIdx });
             }
         }
 
