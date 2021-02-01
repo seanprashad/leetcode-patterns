57a0c0860f3c9a5d766d707204eb10e0fb38aabb
Sean Prashad <S.Prashad@outlook.com>
2021-01-31 22:00:04 -0500

    Update 621_Task_Scheduler.java

--- Greedy/621_Task_Scheduler.java
+++ Greedy/621_Task_Scheduler.java
@@ -1,35 +1,45 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int result = 0;
-
-        int[] map = new int[26];
-        for (char t : tasks) {
-            if (t > 0) {
-                map[t - 'A']++;
-            }
+        if (tasks == null || tasks.length == 0) {
+            return 0;
         }
 
-        Arrays.sort(map);
+        Map<Character, Integer> hm = new HashMap<>();
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
+                (pair1, pair2) -> pair2.getValue() - pair1.getValue());
 
-        while (map[25] > 0) {
-            int iterations = 0;
+        for (char c : tasks) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        }
+        pq.addAll(hm.entrySet());
 
-            while (iterations <= n) {
-                if (map[25] == 0) {
-                    break;
+        int cycles = 0;
+
+        while (!pq.isEmpty()) {
+            List<Map.Entry<Character, Integer>> temp = new ArrayList<>();
+            int emptyTaskSlots = n + 1;
+
+            while (emptyTaskSlots > 0 && !pq.isEmpty()) {
+                --emptyTaskSlots;
+                ++cycles;
+
+                Map.Entry<Character, Integer> entry = pq.poll();
+                entry.setValue(entry.getValue() - 1);
+
+                if (entry.getValue() > 0) {
+                    temp.add(entry);
                 }
-
-                if (iterations < 26 && map[25 - iterations] > 0) {
-                    map[25 - iterations]--;
-                }
-
-                ++iterations;
-                ++result;
             }
 
-            Arrays.sort(map);
+            pq.addAll(temp);
+
+            if (pq.isEmpty()) {
+                break;
+            }
+
+            cycles += emptyTaskSlots;
         }
 
-        return result;
+        return cycles;
     }
 }
