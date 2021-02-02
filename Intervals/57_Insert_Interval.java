3a852e8c6efaca91778fa4e6359dce6d17d73c71
Sean Prashad <S.Prashad@outlook.com>
2021-02-01 21:24:25 -0500

    Update 57_Insert_Interval.java

--- Intervals/57_Insert_Interval.java
+++ Intervals/57_Insert_Interval.java
@@ -4,22 +4,23 @@ class Solution {
         int idx = 0;
 
         while (idx < intervals.length && intervals[idx][1] < newInterval[0]) {
-            result.add(intervals[idx++]);
+            result.add(intervals[idx]);
+            ++idx;
         }
 
         while (idx < intervals.length && intervals[idx][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(intervals[idx][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[idx][1], newInterval[1]);
-
+            newInterval[0] = Math.min(newInterval[0], intervals[idx][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[idx][1]);
             ++idx;
         }
 
         result.add(newInterval);
 
         while (idx < intervals.length) {
-            result.add(intervals[idx++]);
+            result.add(intervals[idx]);
+            ++idx;
         }
 
-        return result.toArray(new int[result.size()][]);
+        return result.toArray(new int[0][]);
     }
 }
