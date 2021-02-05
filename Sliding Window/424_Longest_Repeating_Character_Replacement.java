856ae79b75d512d99ec86aea84ecc6c519a1d333
Sean Prashad <S.Prashad@outlook.com>
2021-02-04 22:30:46 -0500

    Update 424_Longest_Repeating_Character_Replacement.java

--- Sliding Window/424_Longest_Repeating_Character_Replacement.java
+++ Sliding Window/424_Longest_Repeating_Character_Replacement.java
@@ -1,14 +1,14 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] map = new int[26];
-        int start = 0, maxCharCount = 0, result = 0;
+        int[] hm = new int[26];
+        int maxCharCount = 0, result = 0;
 
-        for (int end = 0; end < s.length(); end++) {
-            map[s.charAt(end) - 'A']++;
-            maxCharCount = Math.max(maxCharCount, map[s.charAt(end) - 'A']);
+        for (int start = 0, end = 0; end < s.length(); end++) {
+            hm[s.charAt(end) - 'A']++;
+            maxCharCount = Math.max(maxCharCount, hm[s.charAt(end) - 'A']);
 
-            while (end - start + 1 - maxCharCount > k) {
-                map[s.charAt(start) - 'A']--;
+            if (end - start + 1 - maxCharCount > k) {
+                hm[s.charAt(start) - 'A']--;
                 ++start;
             }
 
