6afca2373ca2c52a39b0359211a40abf42d61193
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2021-02-23 15:05:48 -0500

    Update 253_Meeting_Rooms_II.java

--- Intervals/253_Meeting_Rooms_II.java
+++ Intervals/253_Meeting_Rooms_II.java
@@ -6,19 +6,20 @@ class Solution {
 
         Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
 
-        PriorityQueue<Integer> rooms = new PriorityQueue<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((i1, i2) -> i1[1] - i2[1]);
 
         for (int[] interval : intervals) {
-            int startTime = interval[0];
-            int endTime = interval[1];
+            if (!pq.isEmpty()) {
+                int[] endsNext = pq.poll();
 
-            if (!rooms.isEmpty() && rooms.peek() <= startTime) {
-                rooms.poll();
+                if (interval[0] < endsNext[1]) {
+                    pq.offer(endsNext);
+                }
             }
 
-            rooms.offer(endTime);
+            pq.offer(interval);
         }
 
-        return rooms.size();
+        return pq.size();
     }
 }
