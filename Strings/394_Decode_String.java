290ca2fcf756687cdac4ac525f81ed2045ab22a0
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2021-03-10 15:59:40 -0500

    Update 394_Decode_String.java

--- Strings/394_Decode_String.java
+++ Strings/394_Decode_String.java
@@ -1,34 +1,37 @@
 class Solution {
     public String decodeString(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        Stack<StringBuilder> sbStk = new Stack<>();
         Stack<Integer> multipliers = new Stack<>();
-        Stack<StringBuilder> strings = new Stack<>();
-
         StringBuilder result = new StringBuilder();
-        int idx = 0, multiplier = 0;
+        int multiplier = 0;
 
-        while (idx < s.length()) {
-            if (Character.isDigit(s.charAt(idx))) {
-                multiplier = multiplier * 10 + s.charAt(idx) - '0';
-            } else if (s.charAt(idx) == '[') {
-                strings.push(result);
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                multiplier = multiplier * 10 + c - '0';
+            } else if (c == '[') {
+                sbStk.push(result);
                 multipliers.push(multiplier);
 
-                multiplier = 0;
                 result = new StringBuilder();
-            } else if (s.charAt(idx) == ']') {
-                StringBuilder temp = new StringBuilder(strings.pop());
-                int repetitions = multipliers.pop();
+                multiplier = 0;
+            } else if (c == ']') {
+                StringBuilder previousString = sbStk.pop();
+                int prevMultiplier = multipliers.pop();
 
-                for (int i = 0; i < repetitions; i++) {
-                    temp.append(result);
+                for (int j = 0; j < prevMultiplier; j++) {
+                    previousString.append(result);
                 }
 
-                result = temp;
+                result = previousString;
             } else {
-                result.append(s.charAt(idx));
+                result.append(c);
             }
-
-            ++idx;
         }
 
         return result.toString();
