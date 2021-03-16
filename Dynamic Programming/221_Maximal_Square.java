3d7433f436681c83af9b05f802af90100b8d4ff2
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2021-03-15 23:39:26 -0400

    Update 221_Maximal_Square.java

--- Dynamic Programming/221_Maximal_Square.java
+++ Dynamic Programming/221_Maximal_Square.java
@@ -1,26 +1,18 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
-            return 0;
-        }
+        int sideLen = 0;
 
-        int result = 0;
-        int[][] dp = new int[matrix.length][matrix[0].length];
+        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = matrix[i][j] - '0';
-                } else {
-                    if (matrix[i][j] == '1') {
-                        dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
-                    }
+        for (int i = 1; i <= matrix.length; i++) {
+            for (int j = 1; j <= matrix[0].length; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    sideLen = Math.max(sideLen, dp[i][j]);
                 }
-
-                result = Math.max(result, dp[i][j]);
             }
         }
 
-        return result * result;
+        return sideLen * sideLen;
     }
 }
