dd6226aff49862a0a967e077ccb5636257137dc6
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2021-03-21 22:46:58 -0400

    Update 697_Degree_of_an_Array.java

--- Arrays/697_Degree_of_an_Array.java
+++ Arrays/697_Degree_of_an_Array.java
@@ -1,27 +1,18 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        Map<Integer, int[]> idxMap = new HashMap<>();
-        List<Integer> mostFreq = new ArrayList<>();
-
-        int maxFreq = 0, result = Integer.MAX_VALUE;
+        Map<Integer, Integer> indexes = new HashMap<>();
+        Map<Integer, Integer> frequency = new HashMap<>();
+        int result = 0, maxFrequency = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            int[] indices = idxMap.getOrDefault(nums[i], new int[2]);
+            indexes.putIfAbsent(nums[i], i);
+            frequency.put(nums[i], frequency.getOrDefault(nums[i], 0) + 1);
 
-            if (idxMap.containsKey(nums[i])) {
-                indices[0]++;
-            } else {
-                indices[0] = 1;
-                indices[1] = i;
-            }
-
-            idxMap.put(nums[i], indices);
-
-            if (indices[0] > maxFreq) {
-                maxFreq = indices[0];
-                result = i - indices[1] + 1;
-            } else if (indices[0] == maxFreq) {
-                result = Math.min(result, i - indices[1] + 1);
+            if (frequency.get(nums[i]) > maxFrequency) {
+                maxFrequency = frequency.get(nums[i]);
+                result = i - indexes.get(nums[i]) + 1;
+            } else if (frequency.get(nums[i]) == maxFrequency) {
+                result = Math.min(result, i - indexes.get(nums[i]) + 1);
             }
         }
 
