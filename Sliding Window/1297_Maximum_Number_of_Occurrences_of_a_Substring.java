0fd93a95c9516bcc6b8d9caa852e9b4e9961b86b
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2021-03-25 00:28:50 -0400

    Update 1297_Maximum_Number_of_Occurrences_of_a_Substring.java

--- Sliding Window/1297_Maximum_Number_of_Occurrences_of_a_Substring.java
+++ Sliding Window/1297_Maximum_Number_of_Occurrences_of_a_Substring.java
@@ -1,27 +1,30 @@
 class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
-        int result = 0;
-        Map<String, Integer> freq = new HashMap<>();
+        Map<Character, Integer> m = new HashMap<>();
+        Map<String, Integer> substrings = new HashMap<>();
+        int result = 0, left = 0, right = 0;
 
-        for (int i = 0; i < s.length() - minSize + 1; i++) {
-            String str = s.substring(i, i + minSize);
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            m.put(r, m.getOrDefault(r, 0) + 1);
+            ++right;
 
-            if (isValid(str, maxLetters)) {
-                freq.put(str, freq.getOrDefault(str, 0) + 1);
-                result = Math.max(result, freq.get(str));
+            if (right - left > minSize) {
+                char l = s.charAt(left);
+                m.put(l, m.get(l) - 1);
+                if (m.get(l) == 0) {
+                    m.remove(l);
+                }
+                ++left;
+            }
+
+            if (m.size() <= maxLetters && right - left >= minSize) {
+                String substr = s.substring(left, right);
+                substrings.put(substr, substrings.getOrDefault(substr, 0) + 1);
+                result = Math.max(result, substrings.get(substr));
             }
         }
 
         return result;
-    }
-
-    private boolean isValid(String s, int maxLetters) {
-        Set<Character> set = new HashSet<>();
-
-        for (char c : s.toCharArray()) {
-            set.add(c);
-        }
-
-        return set.size() <= maxLetters;
     }
 }
