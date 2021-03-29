1a83d293838ae0876cedae6dc3b5ba06e01ffdb5
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2021-03-28 22:19:55 -0400

    Update 1348_Tweet_Counts_Per_Frequency.java

--- Design/1348_Tweet_Counts_Per_Frequency.java
+++ Design/1348_Tweet_Counts_Per_Frequency.java
@@ -1,40 +1,45 @@
 class TweetCounts {
-    private Map<String, TreeSet<Integer>> tweets;
+    private Map<String, TreeMap<Integer, Integer>> m;
 
     public TweetCounts() {
-        tweets = new HashMap<>();
+        m = new HashMap<>();
     }
 
     public void recordTweet(String tweetName, int time) {
-        tweets.putIfAbsent(tweetName, new TreeSet<>());
-        tweets.get(tweetName).add(time);
+        m.putIfAbsent(tweetName, new TreeMap<>());
+        TreeMap<Integer, Integer> times = m.get(tweetName);
+        times.put(time, times.getOrDefault(time, 0) + 1);
+        m.put(tweetName, times);
     }
 
     public List<Integer> getTweetCountsPerFrequency(String freq, String tweetName, int startTime, int endTime) {
-        List<Integer> result = new ArrayList<>();
+        if (!m.containsKey(tweetName)) {
+            Collections.emptyList();
+        }
 
-        int delta = calcDelta(freq);
+        List<Integer> result = new ArrayList<>();
+        TreeMap<Integer, Integer> times = m.get(tweetName);
+        int delta = calculateDelta(freq);
         int intervalStart = startTime, intervalEnd = intervalStart + delta;
-        TreeSet<Integer> times = tweets.get(tweetName);
 
         while (intervalStart <= endTime) {
-            SortedSet<Integer> timesSubset = times.subSet(intervalStart, Math.min(intervalEnd, endTime + 1));
-            int intervalCount = 0;
+            // SortedMap<Integer, Integer> timesSubset = times.subMap(intervalStart, Math.min(intervalEnd, endTime + 1));
 
-            for (int frequency : timesSubset) {
-                intervalCount += 1;
+            int count = 0;
+            for (Map.Entry<Integer, Integer> entry : times.subMap(intervalStart, Math.min(intervalEnd, endTime + 1))
+                    .entrySet()) {
+                count += entry.getValue();
             }
 
-            result.add(intervalCount);
-
-            intervalStart = intervalEnd;
-            intervalEnd = intervalStart + delta;
+            result.add(count);
+            intervalStart += delta;
+            intervalEnd += delta;
         }
 
         return result;
     }
 
-    private int calcDelta(String freq) {
+    private int calculateDelta(String freq) {
         if (freq.equals("minute")) {
             return 60;
         } else if (freq.equals("hour")) {
