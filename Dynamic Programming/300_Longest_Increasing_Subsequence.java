56759410cf214056a083cde091bf300809760921
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2021-04-06 15:01:52 -0400

    Update 300_Longest_Increasing_Subsequence.java

--- Dynamic Programming/300_Longest_Increasing_Subsequence.java
+++ Dynamic Programming/300_Longest_Increasing_Subsequence.java
@@ -1,33 +1,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        int len = 0;
+        List<Integer> piles = new ArrayList<>();
 
         for (int num : nums) {
-            int idx = binarySearch(dp, 0, len, num);
-            dp[idx] = num;
-
-            if (idx == len) {
-                ++len;
+            int idx = Collections.binarySearch(piles, num);
+            if (idx < 0) {
+                idx = ~idx;
             }
-        }
 
-        return len;
-    }
-
-    private int binarySearch(int[] nums, int low, int high, int target) {
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
+            if (idx == piles.size()) {
+                piles.add(num);
             } else {
-                high = mid;
+                piles.set(idx, num);
             }
         }
 
-        return low;
+        return piles.size();
     }
 }
