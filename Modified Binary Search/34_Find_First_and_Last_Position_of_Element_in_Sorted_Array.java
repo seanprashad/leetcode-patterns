c1c04cead2660ce61be8fa4b636e867fbb3d7b11
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2021-04-20 22:23:06 -0400

    Update 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.java

--- Modified Binary Search/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
+++ Modified Binary Search/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -1,57 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
+        int firstIdx = binarySearch(nums, target, false);
+        int secondIdx = binarySearch(nums, target, true);
+
+        if (firstIdx == -1 || secondIdx == -1) {
             return new int[] { -1, -1 };
         }
 
-        int[] result = new int[2];
-        result[0] = binarySearchHelperLow(nums, target);
-        result[1] = binarySearchHelperHigh(nums, target);
-
-        return result;
+        return new int[] { firstIdx, secondIdx };
     }
 
-    private int binarySearchHelperLow(int[] nums, int target) {
+    private int binarySearch(int[] nums, int target, boolean moveRight) {
         int low = 0, high = nums.length - 1;
-
-        int idx = -1;
+        int candidate = -1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
             if (nums[mid] == target) {
-                idx = mid;
+                candidate = mid;
             }
 
-            if (nums[mid] >= target) {
-                high = mid - 1;
+            if (moveRight) {
+                if (nums[mid] <= target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             } else {
-                low = mid + 1;
+                if (nums[mid] >= target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
             }
         }
 
-        return idx;
-    }
-
-    private int binarySearchHelperHigh(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-
-        int idx = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                idx = mid;
-            }
-
-            if (nums[mid] <= target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        return idx;
+        return candidate;
     }
 }
