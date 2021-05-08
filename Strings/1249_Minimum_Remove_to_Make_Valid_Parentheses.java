763f4bfa00892b4e78d95986a1b87de1b6ac0295
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2021-05-08 19:35:02 -0400

    Update 1249_Minimum_Remove_to_Make_Valid_Parentheses.java

--- Strings/1249_Minimum_Remove_to_Make_Valid_Parentheses.java
+++ Strings/1249_Minimum_Remove_to_Make_Valid_Parentheses.java
@@ -1,30 +1,36 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        if (s == null || s.length() == 0) { return new String(); }
-        
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> st = new Stack<>();
-        Set<Integer> invalidIndices = new HashSet<>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            
-            if (c == '(') { st.push(i); }
-            else if (c ==')') {
-                if (st.isEmpty()) { invalidIndices.add(i); }
-                else { st.pop(); }
-            }
+        if (s == null || s.isEmpty()) {
+            return "";
         }
-        
-        while (!st.isEmpty()) { invalidIndices.add(st.pop()); }
+
+        StringBuilder sb = new StringBuilder();
+        int counter = 0;
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            
-            if (invalidIndices.contains(i)) { continue; }
+
+            if (c == '(') {
+                ++counter;
+            } else if (c == ')') {
+                if (counter == 0) {
+                    continue;
+                }
+                --counter;
+            }
+
             sb.append(c);
         }
-            
+
+        for (int i = sb.length() - 1; i >= 0; i--) {
+            char c = sb.charAt(i);
+
+            if (c == '(' && counter > 0) {
+                --counter;
+                sb.deleteCharAt(i);
+            }
+        }
+
         return sb.toString();
     }
 }
