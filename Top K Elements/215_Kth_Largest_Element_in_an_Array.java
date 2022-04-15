0c2073a5fade442b93757c25e17794d2a84fbb0a
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2022-04-14 22:05:48 -0400

    Update 215_Kth_Largest_Element_in_an_Array.java

--- Top K Elements/215_Kth_Largest_Element_in_an_Array.java
+++ Top K Elements/215_Kth_Largest_Element_in_an_Array.java
@@ -1,46 +1,33 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        int low = 0, high = nums.length - 1;
         k = nums.length - k;
-
-        while (low < high) {
-            int position = quickSelect(nums, low, high);
-
-            if (position == k) {
-                break;
-            } else if (position < k) {
-                low = position + 1;
-            } else {
-                high = position - 1;
-            }
-        }
-
-        return nums[k];
+        return quickSelect(nums, 0, nums.length - 1, k);
     }
 
-    private int quickSelect(int[] nums, int low, int high) {
+    private int quickSelect(int[] nums, int low, int high, int k) {
         int idx = low, pivot = high;
-        int pivot_value = nums[pivot];
 
         for (int i = low; i < high; i++) {
-            if (nums[i] < pivot_value) {
+            if (nums[i] <= nums[pivot]) {
                 swap(nums, i, idx);
                 ++idx;
             }
         }
 
-        swap(nums, idx, high);
-        return idx;
+        swap(nums, idx, pivot);
+
+        if (idx == k) {
+            return nums[idx];
+        } else if (idx < k) {
+            return quickSelect(nums, idx + 1, high, k);
+        } else {
+            return quickSelect(nums, low, idx - 1, k);
+        }
     }
 
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-        return;
     }
 }
