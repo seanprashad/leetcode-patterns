bf48f06a08053076f23a5a26f9b2445761f3be03
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2022-07-29 17:26:23 -0400

    Complete workflow automation to gh-pages

--- src/components/Acknowledgements/index.js
+++ src/components/Acknowledgements/index.js
@@ -32,7 +32,7 @@ const Acknowledgements = () => {
           <Card>
             <CardImg top width="100%" src={Blind} alt="Blind 75 Question" />
             <CardBody>
-              <CardTitle>Blind 75</CardTitle>
+              <CardTitle>Blind Curated 75 Question List</CardTitle>
               <CardSubtitle>
                 <NavLink
                   target="_blank"
