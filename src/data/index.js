bdbb042f1514a5bf836e133226ec338020446073
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2022-08-07 17:38:57 -0400

    add back company filter (#227)

--- src/data/index.js
+++ src/data/index.js
@@ -3,5 +3,9 @@ import questions from './questions.json';
 const sortBy = { Easy: 0, Medium: 1, Hard: 2 };
 const { updated, data } = questions;
 
+for (let i = 0; i < data.length; i += 1) {
+  data[i].companyNames = data[i].companies.map(company => company.name);
+}
+
 export { updated };
 export default data.sort((a, b) => sortBy[a.difficulty] - sortBy[b.difficulty]);
