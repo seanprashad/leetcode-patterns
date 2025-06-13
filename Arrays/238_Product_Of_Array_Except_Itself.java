22790126cb1b388f3f72625c599d4d8ce9a07c61
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2025-06-13 08:56:22 -0400

    Update `Arrays/238_Product_Of_Array_Except_Itself.java`

--- Arrays/238_Product_Of_Array_Except_Itself.java
+++ Arrays/238_Product_Of_Array_Except_Itself.java
@@ -1,35 +1,16 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
-        int product = 1;
-        int zeroIdx = -1;
-        boolean hasZero = false;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                if (hasZero) {
-                    return new int[nums.length];
-                }
-
-                zeroIdx = i;
-                hasZero = true;
-            } else {
-                result[i] = product;
-                product *= nums[i];
-            }
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
         }
 
-        if (hasZero) {
-            Arrays.fill(result, 0);
-            result[zeroIdx] = product;
-            return result;
-        }
-
-        product = 1;
-
+        int r = 1;
         for (int i = nums.length - 1; i >= 0; i--) {
-            result[i] *= product;
-            product *= nums[i];
+            result[i] *= r;
+            r *= nums[i];
         }
 
         return result;
