1f5df3754f7dfc3c7af8c5f48025b793b0231424
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2025-07-18 19:22:36 -0400

    Update `Topological Sort/269_Alien_Dictionary.java`

--- Topological Sort/269_Alien_Dictionary.java
+++ Topological Sort/269_Alien_Dictionary.java
@@ -1,63 +1,55 @@
 class Solution {
     public String alienOrder(String[] words) {
-        if (words == null || words.length == 0) {
-            return "";
-        }
-
         Map<Character, List<Character>> graph = new HashMap<>();
-        Map<Character, Integer> inorder = new HashMap<>();
+        Map<Character, Integer> counts = new HashMap<>();
 
-        for (String w : words) {
-            for (char c : w.toCharArray()) {
-                graph.putIfAbsent(c, new ArrayList<>());
-                inorder.putIfAbsent(c, 0);
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                graph.put(c, new ArrayList<>());
+                counts.put(c, 0);
             }
         }
 
         for (int i = 0; i < words.length - 1; i++) {
-            String word1 = words[i], word2 = words[i + 1];
+            String w1 = words[i], w2 = words[i + 1];
+            if (w1.length() > w2.length() && w1.startsWith(w2)) { return ""; }
 
-            if (word1.length() > word2.length() && word1.startsWith(word2)) {
-                return "";
-            }
-
-            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
-                char c1 = word1.charAt(j), c2 = word2.charAt(j);
+            for (int j = 0; j < Math.min(w1.length(), w2.length()); j++) {
+                char c1 = w1.charAt(j), c2 = w2.charAt(j);
 
                 if (c1 != c2) {
                     graph.get(c1).add(c2);
-                    inorder.put(c2, inorder.get(c2) + 1);
+                    counts.put(c2, counts.get(c2) + 1);
                     break;
                 }
             }
         }
 
-        Queue<Character> q = new LinkedList<>();
         StringBuilder sb = new StringBuilder();
+        Queue<Character> q = new LinkedList<>();
 
-        for (Character c : inorder.keySet()) {
-            if (inorder.get(c) == 0) {
+        for (char c : counts.keySet()) {
+            if (counts.get(c) == 0) {
                 q.offer(c);
             }
         }
 
         while (!q.isEmpty()) {
-            char c = q.poll();
-            sb.append(c);
+            char curr = q.poll();
+            sb.append(curr);
 
-            for (char neighbour : graph.get(c)) {
-                inorder.put(neighbour, inorder.get(neighbour) - 1);
+            for (char neighbour : graph.get(curr)) {
+                int newCount = counts.get(neighbour) - 1;
+                counts.put(neighbour, newCount);
 
-                if (inorder.get(neighbour) == 0) {
+                if (newCount == 0) {
                     q.offer(neighbour);
                 }
             }
         }
 
-        if (sb.length() < inorder.size()) {
-            return "";
-        }
-        ;
+        if (sb.length() < counts.size()) { return ""; }
+
         return sb.toString();
     }
 }
