1df15e1ff3acd6e107058d17399d51b1c1d08b98
Sean Prashad <13009507+seanprashad@users.noreply.github.com>
2026-03-07 15:42:49 -0500

    Render dark mode title logo first

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,33 +1,20 @@
-"use client";
-
-import { useSyncExternalStore } from "react";
-
-function subscribe(callback: () => void) {
-  const observer = new MutationObserver(callback);
-  observer.observe(document.documentElement, {
-    attributes: true,
-    attributeFilter: ["class"],
-  });
-  return () => observer.disconnect();
-}
-
-function getSnapshot() {
-  return document.documentElement.classList.contains("dark");
-}
-
-function getServerSnapshot() {
-  return false;
-}
-
 export default function Logo() {
-  const dark = useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
   return (
-    // eslint-disable-next-line @next/next/no-img-element
-    <img
-      src={dark ? `${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/images/logo-dark.png` : `${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/images/logo-light.png`}
-      alt="Leetcode Patterns"
-      className="h-8 sm:h-10"
-    />
+    <>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={`${basePath}/images/logo-light.png`}
+        alt="Leetcode Patterns"
+        className="h-8 dark:hidden sm:h-10"
+      />
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={`${basePath}/images/logo-dark.png`}
+        alt="Leetcode Patterns"
+        className="hidden h-8 dark:block sm:h-10"
+      />
+    </>
   );
 }
