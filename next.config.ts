bc06467f7051e57562876a21b5b7d4475e80de15
Sean Prashad <13009507+seanprashad@users.noreply.github.com>
2026-03-06 20:45:34 -0500

    Remove trailing slash

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: false,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? "",
   images: { unoptimized: true },
 };
