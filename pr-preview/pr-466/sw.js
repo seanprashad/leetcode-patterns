8b1e322e366a66197fb2bb4382cff74a60a0c364
seanprashad <seanprashad@users.noreply.github.com>
2026-03-08 18:38:13 +0000

    Deploy preview for PR 466 🛫

---
+++ pr-preview/pr-466/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then((clients) => clients.forEach((c) => c.navigate(c.url)))
+  );
+});
